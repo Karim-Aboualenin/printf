2fc64a89cde9701882daffdb4a626a992798f7a2
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-18 17:38:29 +0300

    0

--- main.c
+++ main.c
@@ -9,8 +9,6 @@
  */
 int main(void)
 {
-     _printf("Let's try to printf a simple sentence.\n");
-    _printf("Character:[%c]\n", 'H');
   _printf("String:[%s]\n", "I am a string !");
     return(0);
 }
