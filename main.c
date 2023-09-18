c54030279b6a77996fc47bc18ef014aa4836c62c
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-18 22:21:58 +0300

    0

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(void)
     int len2;
 len1 = _printf("Let's try to printf a simple sentence.\n");
 len2 = _printf("Character:[%c]\n", 'H');
- printf("Length:[%d, %i]\n", len2, len2);
+ printf("Length:[%d, %i]\n", len1, len2);
  printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
 _printf("String:[%s]\n", "I am a string !");
