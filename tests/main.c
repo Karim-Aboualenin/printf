44eef760aa5bae10f389b11eaade54c2fe522a38
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-18 21:47:58 +0300

    0

--- tests/main.c
+++ tests/main.c
@@ -9,8 +9,7 @@
  */
 int main(void)
 {
-    int len;
-    len = _printf("Let's try to printf a simple sentence.\n");
-    printf("Length:[%d, %i]\n", len, len);
+      _printf("Character:[%c]\n", 'H');
+  _printf("String:[%s]\n", "I am a string !");
     return(0);
 }
