a5fa92b0c7ddc20eb3a7cb8b4f1a2331de43d8a6
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-18 17:22:04 +0300

    0

--- printf.c
+++ printf.c
@@ -11,19 +11,16 @@
 int _printf(const char *format, ...)
 {
 int i = 0, length = 0;
-char *buffer;
 
 va_list args;
 if (format == NULL)
 return (-1);
-buffer = malloc(sizeof(char) * 1024);
-if (buffer == NULL)
-return (-1);
+
 while (format[i] != '\0')
 {
 if (format[i] != '%')
 {
-buffer[i] = format[i];
+write(1, format[i], 1);
 i++, length++;
 }
 else /*if % found*/
@@ -31,19 +28,17 @@ else /*if % found*/
 i++;
 if (format[i] == '%')
 {
-buffer[i] = format[i];
+write(1, format[i], 1);
 i++, length++;
 }
 if (format[i] == '\0')
 {
 va_end(args);
-free(buffer);
 return (-1);
 }
 else
 get_print_function(format[i]);
 }
 }
-write(1, buffer, length);
 return (length);
 }
