92af96ac7d92d0fd177c3b30a538866a3d4f3d5f
abdallah <abdallahanwar899@gmail.com>
2023-09-19 18:23:46 +0300

    00

--- printf.c
+++ printf.c
@@ -10,91 +10,57 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;  /* Count of characters printed */
-    va_list args;   /* Variable arguments list */
-
-    va_start(args, format);  /* Initialize the va_list */
-
-    while (format && *format)
-    {
-        if (*format != '%')
-        {
-            /* Print non-% characters directly */
-            write(1, format, 1);  /* Use write to output a single character */
-            count++;
-        }
-        else
-        {
-            /* Handle conversion specifiers */
-            format++;  /* Move past '%' */
-            switch (*format)
-            {
-                case 'c':
-                    print_char(args, &count);
-                    break;
-                case 's':
-                    print_string(args, &count);
-                    break;
-                case 'd':
-                case 'i':
-                    print_integer(args, &count);
-                    break;
-                case '%':
-                    write(1, "%", 1);  /* Use write to output the percent character */
-                    count++;
-                    break;
-                default:
-                    /* Unknown specifier, ignore */
-                    write(1, "%", 1);  /* Use write to output the percent character */
-                    write(1, format, 1);  /* Use write to output the unknown character */
-                    count += 2;
-            }
-        }
-        format++;
-    }
-
-    va_end(args);  /* Cleanup the va_list */
-    return count;
-}
-
-/**
- * print_char - Print a character.
- * @args: The va_list containing the character argument.
- * @count: Pointer to the character count.
- */
-static void print_char(va_list args, int *count)
+int count = 0;  /* Count of characters printed */
+va_list args;   /* Variable arguments list */
+va_start(args, format);  /* Initialize the va_list */
+while (format && *format)
 {
-    char c = va_arg(args, int);
-    write(1, &c, 1);  /* Use write to output the character */
-    (*count)++;
-}
-
-/**
- * print_string - Print a string.
- * @args: The va_list containing the string argument.
- * @count: Pointer to the character count.
- */
-static void print_string(va_list args, int *count)
+if (*format != '%')
 {
-    const char *str = va_arg(args, const char *);
-    if (str == NULL)
-        str = "(null)";
-    while (*str)
-    {
-        write(1, str, 1);  /* Use write to output a single character */
-        str++;
-        (*count)++;
-    }
+write(1, format, 1);  /* Use write to output a single character */
+count++;
 }
-
-/**
- * print_integer - Print an integer.
- * @args: The va_list containing the integer argument.
- * @count: Pointer to the character count.
- */
-static void print_integer(va_list args, int *count)
+else
 {
-    int num = va_arg(args, int);
-    dprintf(1, "%d", num);  /* Use dprintf to print the integer */
-    (*count)++;
+format++;  /* Move past '%' */
+if (*format == 'd' || *format == 'i')
+{
+int num = va_arg(args, int);
+dprintf(1, "%d", num);  /* Use dprintf to print the integer */
+count++;
+}
+else if (*format == 'c')
+{
+char c = va_arg(args, int);
+write(1, &c, 1);  /* Use write to output the character */
+count++;
+}
+else if (*format == 's')
+{
+const char *str = va_arg(args, const char *);
+if (str == NULL)
+str = "(null)";
+while (*str)
+{
+write(1, str, 1);
+str++;
+count++;
+}
+}
+else if (*format == '%')
+{
+write(1, "%", 1);  /* Use write to output the percent character */
+count++;
+}
+else
+{
+write(1, "%", 1);  /* Use write to output the percent character */
+write(1, format, 1);  /* Use write to output the unknown character */
+count += 2;
+}
+}
+format++;
+}
+va_end(args);  /* Cleanup the va_list */
+return (count);
 }
