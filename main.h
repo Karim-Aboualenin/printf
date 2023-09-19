06630a3483d776df7ede88a28ccd7ccaf9fc4c66
abdallah <abdallahanwar899@gmail.com>
2023-09-19 18:26:28 +0300

    00

--- main.h
+++ main.h
@@ -28,23 +28,10 @@ char specifier;
 int (*func)(va_list);
 } specifiers_t;
 
-int _strlen(char *s);
-
-struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-};
-typedef struct fmt fmt_t;
-
+int _strlen(char *s);	
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 		va_list list, char buffer[], int flags, int width, int precision, int size);
-
-/* Function prototypes for handling each specifier */
-static void print_char(va_list args, int *count);
-static void print_string(va_list args, int *count);
-static void print_integer(va_list args, int *count);
 
 int _printf(const char *format, ...);
 int (*get_print_function(char s))(va_list);
@@ -54,28 +41,4 @@ int get_flag (const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
-
-
-int print_percent(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_int(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_binary(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_unsigned(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_octal(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_hexadecimal(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_hexa_upper(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_pointer(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_non_printable(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
-int print_rot13string(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_reverse(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
 #endif
