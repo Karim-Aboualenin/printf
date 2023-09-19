f421fb4e8041907d41cf7fd43e42055c10d6262c
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-19 20:33:53 +0300

    1

--- main.c
+++ main.c
@@ -9,7 +9,13 @@
  */
 int main(void)
 {
-      _printf("number:[%d]\n", 10);
-  _printf("String:[%s]\n", "I am a string !");
+  int len;
+    int len2;
+len =  _printf("number:[%d]\n", "28474");
+len2 = printf("number:[%d]\n", "28474");
+ printf("Length:[%d, %i]\n", len, len2);
+_printf("Character:[%c]\n", 'H');
+printf("Character:[%c]\n", 'H');
+
     return(0);
 }
