fd831d0650fada24939ab158c66fc45d2fbd8f3b
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-19 21:06:45 +0300

    0

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+  int len;
+  int len2;
+  int len3;
+  int len4;
+len =  _printf("char:[%c]\n", 'h');
+len2 = printf("char:[%c]\n", 'h');
+len3 = _printf("string:[%s]\n", "hello, from another world");
+len4 = printf("string:[%s]\n", "hello, from another world");
+ printf("Length:[%d, %i]\n", len, len2);
+ printf("Length:[%d, %i]\n", len3, len4);
+return(0);
+}
