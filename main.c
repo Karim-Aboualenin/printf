90009d034921c376150adc731648bca7e747713c
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-19 21:11:22 +0300

    1

--- main.c
+++ main.c
@@ -13,11 +13,9 @@ int main(void)
   int len2;
   int len3;
   int len4;
-len = _printf("%% 532");
-len2 = printf("%% 532");
-len3 = _printf("string:[%s]\n", "hello, from another world");
-len4 = printf("string:[%s]\n", "hello, from another world");
- printf("Length:[%d, %i]\n", len, len2);
- printf("Length:[%d, %i]\n", len3, len4);
+len = _printf("%%");
+len2 = printf("%%");
+printf("Length:[%d, %i]\n", len, len2);
+printf("Length:[%d, %i]\n", len3, len4);
 return(0);
 }
