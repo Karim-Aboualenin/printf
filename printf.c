125f5317e93cd8bf094c73ebb281d5c87c2273e5
abdallah <abdallahanwar899@gmail.com>
2023-09-19 18:00:46 +0300

    00

--- printf.c
+++ printf.c
@@ -1,47 +1,76 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - a function that produces output according to a format.
- * @format: is a character string. The format string is
- * composed of zero or more directives
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
-*/
-
+ * _printf - Custom printf function with limited functionality.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
 int _printf(const char *format, ...)
 {
-int i = 0, length = 0, length_return = 0;
-int (*func)(va_list);
-va_list args;
-if (format == NULL)
-return (-1);
-va_start(args, format);
-while (format[i] != '\0')
-{
-if (format[i] != '%')
-{
-write(1, &format[i], 1);
-i++, length++;
-}
-else /*if % found*/
-{
-i++;
-if (format[i] == '%')
-{
-write(1, &format[i], 1);
-i++, length++;
-}
-else if (format[i] == '\0')
-{
-va_end(args);
-return (-1);
-}
-else
-{
-func = get_print_function(format[i]);
-length_return = func(args);
-i++;
-length += length_return;
-}}}
-return (length);
+    int count = 0;  /* Count of characters printed */
+    va_list args;   /* Variable arguments list */
+
+    va_start(args, format);  /* Initialize the va_list */
+
+    while (format && *format)
+    {
+        if (*format != '%')
+        {
+            /* Print non-% characters directly */
+            write(1, format, 1);  /* Use write to output a single character */
+            count++;
+        }
+        else
+        {
+            /* Handle conversion specifiers */
+            format++;  /* Move past '%' */
+            if (*format == 'd' || *format == 'i')
+            {
+                /* Integer specifier */
+                int num = va_arg(args, int);
+                dprintf(1, "%d", num);  /* Use dprintf to print the integer */
+                count++;
+            }
+            else if (*format == 'c')
+            {
+                /* Character specifier */
+                char c = va_arg(args, int);
+                write(1, &c, 1);  /* Use write to output the character */
+                count++;
+            }
+            else if (*format == 's')
+            {
+                /* String specifier */
+                const char *str = va_arg(args, const char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);  /* Use write to output a single character */
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                /* Percent specifier */
+                write(1, "%", 1);  /* Use write to output the percent character */
+                count++;
+            }
+            else
+            {
+                /* Unknown specifier, ignore */
+                write(1, "%", 1);  /* Use write to output the percent character */
+                write(1, format, 1);  /* Use write to output the unknown character */
+                count += 2;
+            }
+        }
+        format++;
+    }
+
+    va_end(args);  /* Cleanup the va_list */
+    return count;
 }
