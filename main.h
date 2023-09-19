588a13dfcffc069867f04ff49994fdaa34a22ce2
abdallah <abdallahanwar899@gmail.com>
2023-09-19 16:15:50 +0300

    1

--- main.h
+++ main.h
@@ -30,7 +30,17 @@ int (*func)(va_list);
 
 int _strlen(char *s);
 
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+typedef struct fmt fmt_t;
 
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+		va_list list, char buffer[], int flags, int width, int precision, int size);
+        
 int _printf(const char *format, ...);
 int (*get_print_function(char s))(va_list);
 int print_char(va_list args);
@@ -40,4 +50,30 @@ int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
+int print_char(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_pointer(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_non_printable(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_rot13string(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+int print_reverse(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
 #endif
