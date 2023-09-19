c513fcbcefb483c8ad17794fac8c8e21b192c73c
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-19 21:11:36 +0300

    0

--- main.c
+++ main.c
@@ -11,11 +11,8 @@ int main(void)
 {
   int len;
   int len2;
-  int len3;
-  int len4;
 len = _printf("%%");
 len2 = printf("%%");
 printf("Length:[%d, %i]\n", len, len2);
-printf("Length:[%d, %i]\n", len3, len4);
 return(0);
 }
